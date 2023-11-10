fe0e8963913da397d782e6d5ce1de154544cd713
Elias Armando <armand.g8@gmail.com>
2023-11-10 10:18:33 -0600

    Add pages in module Naruto and update: app.component.html, app.module.ts?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './naruto/pages/main-page.component';
+import { NarutoModule } from './naruto/naruto.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
+  ],
+  exports: [
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NarutoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
